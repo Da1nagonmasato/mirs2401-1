5417ebac17db4a0a7296f02830ad66c56d207cad
Da1nagonmasato <d21136@numazu.kosen-ac.jp>
2024-12-26 18:35:56 +0900

    action.html送れる

---
+++ build/mirs_msgs/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /var/www/html/mirs_msgs/mirs_msgs
+# Build directory: /var/www/html/build/mirs_msgs
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("mirs_msgs__py")
---
+++ build/mirs_msgs/mirs_msgs__py/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /var/www/html/build/mirs_msgs/mirs_msgs__py
+# Build directory: /var/www/html/build/mirs_msgs/mirs_msgs__py
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
